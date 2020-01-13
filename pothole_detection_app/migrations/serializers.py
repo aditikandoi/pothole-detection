78073ed94f5b04cc3eb67d4e1562689ad6f8e783
aditikandoi <kandoiaditi0@gmail.com>
2020-01-13 18:44:53 +0530

    models added serializers added

--- pothole_detection_app/migrations/serializers.py
+++ pothole_detection_app/migrations/serializers.py
@@ -1,10 +1,7 @@
 from rest_framework import serializers
 from .models import Post,Vote,Garbage_User,Vote_table
 
-class VoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Vote
-        fields="__all__"
+
 
 class PostSerializer(serializers.ModelSerializer):
     # votes=VoteSerializer(many=True,required=False)
